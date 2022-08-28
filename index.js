947fc493a0523bbe15e77fc18f652f8314409336
ErikoX <ponotone@gmail.com>
2022-08-28 15:41:47 +0200

    just fixing the whole repo

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, ".", "index.html"));
 });
 
-app.use(express.static(path.join(__dirname, "./dist")));
-app.use(express.static(path.join(__dirname, "./sounds")))
-app.use(express.static(path.join(__dirname, "./sprites")))
+app.use("/dist", express.static(path.join(__dirname, "./dist")));
+app.use("/sounds", express.static(path.join(__dirname, "./sounds")))
+app.use("/sprites", express.static(path.join(__dirname, "./sprites")))
 
 app.listen(8000, () => {
     console.log("Started server! http://localhost:8000");
